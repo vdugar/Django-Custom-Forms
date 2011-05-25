6e5c2493a53f0384b2d04f85edb9861d26aeca23
Vishal Dugar <vishaldugar12@gmail.com>
2011-05-25 21:07:15 +0530

    Update and delete fields

--- static/js/custom_form.js
+++ static/js/custom_form.js
@@ -45,12 +45,15 @@ var elemTypes = {
 	'city':0
 };
 
-var currElemType, currElemIndex, optionCount=1, formTitle="Form",currCategory='';
+var covenience = {
+	//A list of convenience methods
+	
+};
+
+var currElemType, currElemIndex, optionCount=1, formTitle="Form",currCategory='',$prevField, $currField;
 
 $(document).ready(function() {
-    $('#button_remove').toggle();		//Hide remove button when page loads
-	$('#button_add').click(function(){addElement($('#elem_selector').attr('value'))});
-	$('#button_remove').click(removeElement);
+    $('#button_add').click(function(){addElement($('#elem_selector').attr('value'),$prevField)});
 	$('#sbmt').click(submit);
 	$('#button_title').click(updateTitle);
 	
@@ -84,13 +87,14 @@ $(document).ready(function() {
 
 
 var cleanLabel=function(labeltext) {
-	// Strips the * sign from the label text
+	// Strips the * sign from the label text, if present
+	
 	var return_val;
 	if(labeltext.indexOf('*')== -1)
 		return_val = labeltext;
 	else return_val= labeltext.substr(0,labeltext.length-1);	
 	return return_val;
-}
+};
 
 var createLabel=function(labeltext, required) {
 	//Returns an HTML-formatted label, with a red * if the question is required
@@ -99,17 +103,13 @@ var createLabel=function(labeltext, required) {
 	if(!required)
 		return '<p>'+labeltext+'</p>';
 	else return '<p>'+labeltext+'<span class="asterisk">'+'*'+'</span></p>';	
-}
+};
 
-var removeElement = function() {
+var removeField = function() {
 	//removes the selected element from the form by performing .remove() on the wrapper div
 	
-	$currLabelId="label_"+currElemType+"_"+currElemIndex;
-	$currLabel=$('#'+$currLabelId);
-	$currLabel.parent().remove();
-	$("#id_question").attr('value','');
-	currElemType=currElemIndex=0;
-}
+	$(this).parent().remove();
+};
 
 var updateElement=function() {
 	// updates the selected form element
@@ -117,32 +117,12 @@ var updateElement=function() {
 	if($('#id_required').attr('checked'))
 		$lab_text+="*";
 	$('#label_'+currElemType+'_'+currElemIndex).html($lab_text);
-}
-
-var toggleButtons=function() {
-	// toggles the visibility of the remove button, and changes the behaviour of the add button to "update"
-	
-	$button_remove=$('#button_remove');
-	$button_add=$('#button_add');
-	$button_add.unbind('click');
-	if ($button_remove.is(":visible")) {
-		$button_add.attr('value','Add to Form');
-		$button_add.click(function(){addElement($('#elem_selector').attr('value'))});
-	}
-	else {
-		$button_add.attr('value','Update');
-		$button_add.click(updateElement);
-	}
-	$button_remove.toggle();
-}
+};
 
 var onClickLabel=function(event) {
 	// Allows realtime updating of form
 	
 	var $radio_options;
-	if (!($('#button_remove').is(":visible")))
-		toggleButtons();
-	
 	var $curr_label=$(event.target);
 	currElemType=$curr_label.attr('id').split('_')['1'];
 	if(currElemType!='radio')
@@ -152,57 +132,36 @@ var onClickLabel=function(event) {
 	if($curr_label.html().indexOf('*')== -1)
 		$('#id_required').attr('checked','');
 	else $('#id_required').attr('checked','on')	;
-}
+};
 
-var addOption=function(event) {
-	//adds an option to the radio group 
+var addOption=function(option_text) {
+	//adds an option in the form toolbox
 	
 	var $option,$wrap_option;
-	var curr_add_id=$(event.target).attr('id');
-	var curr_add_index=curr_add_id.split('_')[1];
-	$wrap_option=$('<div></div>');
-	$wrap_option.addClass('option_element');
-	$wrap_option.insertAfter($('#id_option_'+curr_add_index).parent());
+	$wrap_option=$('<div></div>').addClass('option_element');
 	$option=$('<input/>', {
 		type:"text",
-		value:"Option",
-		name:"option_"+optionCount,
-		id:"id_option_"+optionCount
+		value:option_text,
 	});
 	$option.appendTo($wrap_option);
-	$('<input/>',{type:'button',value:'+',id:'add_'+optionCount}).click(addOption).appendTo($wrap_option);
-	$('<input/>',{type:'button',value:'-',id:'remove_'+optionCount}).click(removeOption).appendTo($wrap_option);
-	optionCount++;
-	$('<br />').appendTo($wrap_option);
-}
+	$('<input/>',{type:'button',value:'+'}).click(function(){addOption('')}).appendTo($wrap_option);
+	$('<input/>',{type:'button',value:'-'}).click(removeOption).appendTo($wrap_option);
+	$wrap_option.appendTo($('#multi_options'));
+};
 
-var removeOption=function(event) {
+var removeOption=function() {
 	//removes an option from the radio group
 	
-	$(event.target).parent().remove();
-}
+	$(this).parent().remove();
+};
 
 var generateOptions=function() {
 	//Generates the options input fields for multi-select form fields
 	
-	var frag=document.createDocumentFragment(),i,$wrap_option;
-	for(i=1;i<=2;i++) {
-		$wrap_option=$('<div></div>');
-		$wrap_option.addClass('option_element');
-		$option=$('<input/>', {
-			type:"text",
-			value:"Option",
-			name:"option_"+optionCount,
-			id:"id_option_"+optionCount
-		});
-		$option.appendTo($wrap_option);
-		$('<input/>',{type:'button',value:'+',id:'add_'+optionCount}).click(addOption).appendTo($wrap_option);
-		$('<input/>',{type:'button',value:'-',id:'remove_'+optionCount}).click(removeOption).appendTo($wrap_option);
-		optionCount++;
-		frag.appendChild($wrap_option[0]);
+	for(i=1;i<=3;i++) {
+		addOption('');
 	}
-	$('#multi_options')[0].appendChild(frag);
-}
+};	
 
 var getFirst=function(category){
 	//returns the first item corresponding to category
@@ -212,7 +171,7 @@ var getFirst=function(category){
 	else if(category=='Personal')
 		return 'name';
 	else return '';	
-}
+};
 
 var onSelectCategory=function(category) {
 	//Populates the Field selector with the appropriate form fields
@@ -229,22 +188,29 @@ var onSelectCategory=function(category) {
 	
 	//'Initializing' form builder with first field element
 	onSelectElem(getFirst(category));
+};
+
+var clearSpecificOptions=function() {
+	//Removes previous field-specific options, if any
+	
+	var $multi_options=$('#multi_options'),$other_options=$('#other_options');
+	if($multi_options.children().length!=0)
+		$multi_options.empty();
+	if($other_options.children().length!=0)
+		$other_options.empty();
 }
 
 var onSelectElem = function(item) {
 	//Generates the properties fields when a form item is selected from the list
 	
 	//Remove previous field-specific options, if any
-	$multi_options=$('#multi_options'),$other_options=$('#other_options');
-	if($multi_options.children().length!=0)
-		$multi_options.empty();
-	if($other_options.children().length!=0)
-		$other_options.empty();
+	clearSpecificOptions();
+	$('#id_instructions').attr('value','');
+	$('#id_required').attr('checked','');
+	
+	$('div.field_selected').removeClass('field_selected');
 		
-	var $option,$wrap_option,i;
-	if (($('#button_remove').is(":visible")))
-		toggleButtons();
-	var question_text=formElements[currCategory][item]['ques'];
+	var $option,$wrap_option,i, question_text=formElements[currCategory][item]['ques'], $button=$('#button_add');;
 	
 	//Defining actions for generic elements
 	if(item=="radio") 
@@ -275,50 +241,123 @@ var onSelectElem = function(item) {
 	//Defining actions for custom fields	
 	
 	$('#id_question').attr('value',question_text);
-}
+	$prevField=$('div.form_preview :last-child').filter('div.field_wrapper');
+	if($button.attr('value')!='Add to Form')
+		$button.attr('value','Add to Form').unbind('click').click(function(){addElement($('#elem_selector').attr('value'),$prevField)});
+};
 
-var addElement = function(item) {
+var updateField=function() {
+	var curr_field_type=$.data($currField[0],'data').field_type;
+	$prevField=$currField.prev();
+	$currField.remove();
+	$currField=addElement(curr_field_type,$prevField);
+	$currField.addClass('field_selected');
+};
+
+var onSelectField=function() {
+	//Handles clicks on field wrappers
+	
+	clearSpecificOptions();
+	//De-selecting any previously selected field
+	$('div.field_selected').removeClass('field_selected');
+	
+	var $wrap=$(this), $button=$('#button_add');;
+	$wrap.removeClass('field_hover').addClass('field_selected');
+	$wrap.find('.wrapper_button').removeClass('wrapper_button_hover');
+	var field_data=$.data($wrap[0],'data'), question_text=$wrap.find("p").eq(0).text(), $options;
+	$currField=$wrap;
+	
+	if(question_text.indexOf("*")!=-1)
+		$("#id_required").attr('checked','checked');
+	$("#id_question").attr('value',cleanLabel(question_text));
+	$("#id_instructions").attr('value',field_data.help_text);
+	
+	//Adding in field-specific options
+	if(field_data.field_type=='radio'){
+		$options=$wrap.find('div').children().filter('p');
+		$options.each(function(idx,el) {
+			addOption($(el).text());
+		})
+	}
+	else if(field_data.field_type=='dropdown'){
+		$options=$wrap.find('select').children();
+		$options.each(function(idx,el){
+			addOption($(el).html());
+		})
+	}
+	else if(field_data.field_type=='numeric'){
+		var $range_div=$('<div></div>').addClass('toolboxText');
+		$minInput=$('<input/>', {
+			type:"text",
+			value:field_data.min,
+			name:"minVal",
+			id:"id_minVal"
+		});
+		$maxInput=$('<input/>', {
+			type:"text",
+			value:field_data.max,
+			name:"maxVal",
+			id:"id_maxVal"
+		});
+		
+		$range_div.append($('<span>Min</span>')).append($minInput).append('&nbsp;&nbsp;').append($('<span>Max</span>')).append($maxInput);
+		$range_div.appendTo($('#other_options'));
+	}
+	if($button.attr('value')=='Add to Form')
+		$button.attr('value','Update').unbind('click').click(updateField);
+		
+};
+
+var addElement = function(item,$prevField) {
 	
 	// This function adds the selected field to the form. Data like help-text is stored in the wrapper div using jQuery's $.data
 	
 	var i,$new_elem,$new_elem_label, 
-		$wrap=$('<div></div>').addClass('field_wrapper'),
+		$wrap=$('<div></div>').addClass('field_wrapper').hover(function() {
+			if($(this).hasClass('field_selected'))
+				return;
+			$(this).toggleClass('field_hover');
+			$(this).find(".wrapper_button").toggleClass("wrapper_button_hover");
+		}).click(onSelectField),
 		label_text=$('#id_question').attr('value'),
-		help_text=$('#id_instructions').attr('value')
+		help_text=$.trim($('#id_instructions').attr('value')),
+		html_name=item+"_"+elemTypes[item], html_id="id_"+item+"_"+elemTypes[item],
 		data={};
 	
 	$new_elem_label=$(createLabel(label_text,$('#id_required').attr('checked'))).appendTo($wrap);
+	$('<input/>',{type:'button',value:'X'}).click(removeField).addClass("wrapper_button").appendTo($wrap);
 	data.help_text=help_text;
+	data.field_type=item;
 	
 	//Generic fields first
 	if(item=="textField"){
 		$new_elem=$('<input/>', {
 			type:"text",
-			name:"textField_"+elemTypes['textField'],
-			id:'id_textField_'+elemTypes['textField'],
+			name:html_name,
+			id:html_id,
 			size:"30"
 		});
 	}
 	else if(item=="longTextField"){
 		$new_elem=$('<input/>', {
 			type:"text",
-			name:"longTextField_"+elemTypes['longTextField'],
-			id:'id_textField_'+elemTypes['longTextField'],
+			name:html_name,
+			id:html_id,
 			size:"60"
 		});
 	}
 	else if(item=="longAns") {
 		$new_elem=$('<textarea>', {
-			name:"longAns_"+elemTypes['longAns'],
-			id:"id_longAns_"+elemTypes['longAns'],
+			name:html_name,
+			id:html_id,
 			rows:"8",
 			cols:"50"
 		});
 	}
 	else if(item=="reallyLongAns") {
 		$new_elem=$('<textarea>', {
-			name:"reallyLongAns_"+elemTypes['reallyLongAns'],
-			id:"id_reallyLongAns_"+elemTypes['reallyLongAns'],
+			name:html_name,
+			id:html_id,
 			rows:"14",
 			cols:"70"
 		});
@@ -326,13 +365,12 @@ var addElement = function(item) {
 	else if(item=="radio") {
 		var $text_inputs=$('#multi_options input:text'), $one_option;
 		$new_elem=$("<div>", {
-			name:"radio_"+elemTypes["radio"],
-			id:"id_radio_"+elemTypes["radio"],
+			id:html_id
 		});
 		$text_inputs.each(function(idx,el) {
 				$one_option=$('<input>', {
 						type:"radio",
-						name:"radio_"+elemTypes['radio'],
+						name:html_name,
 						value:$(el).attr('value')
 				});	
 				$new_elem.append($("<p>").append($one_option).append($("<span>"+$(el).attr('value')+"</span>")));
@@ -340,8 +378,8 @@ var addElement = function(item) {
 	}
 	else if(item=="dropdown") {
 		$new_elem=$('<select>',{
-			name:"dropdown_"+elemTypes["dropdown"],
-			id:"id_dropdown_"+elemTypes["dropdown"],
+			name:html_name,
+			id:html_id
 		});
 		var $text_inputs=$('#multi_options input:text'), $one_option;
 		$text_inputs.each(function(idx,el) {
@@ -355,18 +393,26 @@ var addElement = function(item) {
 	else if(item=="numeric"){
 		$new_elem=$('<input/>', {
 			type:"text",
-			name:"numeric_"+elemTypes['numeric'],
-			id:'id_numeric_'+elemTypes['numeric'],
+			name:html_name,
+			id:html_id,
 			size:"20"
 		});
 		data.min=$('#id_minVal').attr('value');
 		data.max=$('#id_maxVal').attr('value');
 	}
+	else if(item=="file"){
+		$new_elem=$('<input/>', {
+			type:"file",
+			name:html_name,
+			id:html_id,
+			size:"40"
+		});
+	}
 	else if(item=="name"){
 		$new_elem=$('<input/>', {
 			type:"text",
-			name:"name_"+elemTypes['name'],
-			id:"id_name_"+elemTypes['name']
+			name:html_name,
+			id:html_id
 		});
 		
 		$new_elem.appendTo($wrap);
@@ -417,8 +463,12 @@ var addElement = function(item) {
 	elemTypes[item]++;
 	$new_elem.appendTo($wrap);
 	$.data($wrap[0],'data',data);
-	$wrap.appendTo($(".form_preview"));
-}
+	if($prevField.length==0)
+		$wrap.prependTo($('div.form_preview'));
+	else
+		$wrap.insertAfter($prevField);
+	return $wrap;
+};
 
 var submit=function() {
 	//submits the created form to the server
@@ -469,12 +519,12 @@ var submit=function() {
 	});
 	$('#sbmt').attr("disabled","true")
 	
-}
+};
 
 var updateTitle = function(){
 	//Updates the title for the form
 	
 	formTitle=$("#input_title").attr('value');
 	$("#form_title").html(formTitle);
-}
+};
 
